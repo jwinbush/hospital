2a11d5dda58f2a7f9957727e3fd936aae0c82576
J.M Winbush <jawonwinbush@gmail.com>
2024-09-18 17:12:31 -0400

    Finished making responsive 📲

--- src/app/Home/home/home.component.ts
+++ src/app/Home/home/home.component.ts
@@ -1,19 +1,21 @@
+// home.component.ts
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { SharingService } from 'src/app/services/sharing.service';
+import { faCoffee, faHome } from '@fortawesome/free-solid-svg-icons'; // Import icons
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-
-
 export class HomeComponent implements OnInit {
-
-
   light: any;
   @ViewChild("about", { static: true }) aboutElement!: ElementRef;
+
+  // Declare the icons
+  faCoffee = faCoffee;
+  faHome = faHome;
 
   toggleNav() {
     if (this.light == "light") {
@@ -25,9 +27,9 @@ export class HomeComponent implements OnInit {
 
     this.sharingService.setData(this.light);
   }
+
   constructor(private activeRoute: ActivatedRoute,
     private sharingService: SharingService) { }
-
 
   ngOnInit(): void {
     this.light = "light";
@@ -38,16 +40,14 @@ export class HomeComponent implements OnInit {
     let params = this.activeRoute.snapshot.params;
     //Check if the parameter comes for the about
     if (params !== null && typeof params === 'object' && params.goAbout) {
-
       if (this.aboutElement != undefined) {
         this.aboutElement.nativeElement.scrollIntoView({block: "center", behavior: "smooth" });
       }
-
     }
   }
 
   navToElement(el: any): void {
-    //Scroll into Home componet   
+    // Scroll into Home component
     this.sharingService.navToElement(el);
   }
 }
